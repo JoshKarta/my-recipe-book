b76e59ff42ce57071dd4023accce2c4c4fee4350
Josh Karta <karta.josh.natin@gmail.com>
2026-04-08 19:06:50 -0300

    feat: build generate db schema changes

---
+++ drizzle/0001_mushy_jazinda.sql
@@ -0,0 +1,7 @@
+ALTER TABLE "session" ADD COLUMN "active_organization_id" text;--> statement-breakpoint
+ALTER TABLE "session" ADD COLUMN "active_team_id" text;--> statement-breakpoint
+CREATE INDEX "invitation_organizationId_idx" ON "invitation" USING btree ("organization_id");--> statement-breakpoint
+CREATE INDEX "invitation_email_idx" ON "invitation" USING btree ("email");--> statement-breakpoint
+CREATE INDEX "member_organizationId_idx" ON "member" USING btree ("organization_id");--> statement-breakpoint
+CREATE INDEX "member_userId_idx" ON "member" USING btree ("user_id");--> statement-breakpoint
+CREATE UNIQUE INDEX "organization_slug_uidx" ON "organization" USING btree ("slug");
